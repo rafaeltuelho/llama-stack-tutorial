93df30b75dbaec5cbc95da1948030bea1796127d
Rafael T. C. Soares <rafaelcba@gmail.com>
2025-04-01 17:24:56 -0500

    adding a new prompt

--- 5-rag-with-doclin.py
+++ 5-rag-with-doclin.py
@@ -92,9 +92,9 @@ session_id = rag_agent.create_session("test-session")
 user_prompts = [
     "what are the Example of Expenses after 1 year and after 3 years?",
     "What is the investment Objective for the fund?",
-    "What is the management fees?",
-    "Show me the Example of Expenses as a table?",
-    "List the Average Annual Total Returns for the periods ended 12/31/2024 as a table"
+    # "What is the management fees?",
+    # "Show me the Example of Expenses as a table?",
+    "List the Average Annual Total Returns for the periods ended 12/31/2024 for Class A (before taxes)"
     # "What is the fund's investment strategy?",
     # "What is the fund's risk profile?"
 ]
